f54f2555a33f7c1133b66b9655ec02b9619788ef
Mung'ere <Collins.Mungere@equitybank.co.ke>
2024-07-17 15:59:06 +0300

    project hang man set up

---
+++ src/hangman.kt
@@ -0,0 +1,8 @@
+fun main(){
+    val words = listOf("plate","egg","memo","java","random","name", "word", "repeat","another","one","main","reggae","music","great","kenya","planet","earth","ear","nose")
+val word = ""
+    var guesses = arrayListOf<Char>()
+    var remainingGuesses = 6
+    var mistakes = 0
+
+}
